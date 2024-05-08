fe8e153255c410288597df4b871520b0586d64fe
pruthviraj-gaikwad <pruthviraj162004@gmail.com>
2024-05-08 22:46:52 +0530

    working on gui

---
+++ java_8_5/gui1.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class gui1 {
+    public static void main(String ...args){
+        JOptionPane.showMessageDialog(null,"pruthvi","prem",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
